53056f12b9901f5768dde1d0e759c2dfd658d733
Mahesh <b.mahesh311296@gmail.com>
2024-07-16 15:13:19 +0530

    allowed cross origin

--- App.js
+++ App.js
@@ -14,7 +14,10 @@ app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
 const port = 5000;
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.get("/", (req, res) => {
   const time = new Date().getTime();
   const date = new Date().toDateString();
